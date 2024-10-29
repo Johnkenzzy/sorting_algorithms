4554cd35df059e48ace0a3db036d3cd24f5024dc
johnkenzzy <johnkenumeh@gmail.com>
2024-10-29 03:15:57 -0700

    Completed task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -5,34 +5,42 @@
  * lumoto_partition - divides an array into two partitions
  *
  * @array: array to be partitioned
- * @low_part: lower partition of array
- * @high_part: higher partition of array
+ * @size: size of the array
+ * @low_pt: lower partition of array
+ * @high_pt: higher partition of array
  *
  * Return: the index of partition
  */
-size_t lumoto_partition(int *array, size_t low_part, size_t high_part)
+size_t lumoto_partition(int *array, size_t low_pt, size_t high_pt, size_t size)
 {
 	int pivot;
 	size_t temp, pivot_ndx, i;
 
-	pivot = array[high_part];
-	pivot_ndx = low_part;
+	pivot = array[high_pt];
+	pivot_ndx = low_pt;
 
-	for (i = low_part; i < high_part; i++)
+	for (i = low_pt; i < high_pt; i++)
 	{
 		if (array[i] <= pivot)
 		{
-			temp = array[pivot_ndx];
-			array[pivot_ndx] = array[i];
-			array[i] = temp;
+			if (i != pivot_ndx)
+			{
+				temp = array[pivot_ndx];
+				array[pivot_ndx] = array[i];
+				array[i] = temp;
+				print_array(array, size);
+			}
 			pivot_ndx++;
 		}
 	}
 
-	print_array(array, high_part + 1);
-	temp = array[pivot_ndx];
-	array[pivot_ndx] = array[high_part];
-	array[high_part] = temp;
+	if (pivot_ndx != high_pt)
+	{
+		temp = array[pivot_ndx];
+		array[pivot_ndx] = array[high_pt];
+		array[high_pt] = temp;
+		print_array(array, size);
+	}
 
 	return (pivot_ndx);
 }
@@ -42,26 +50,28 @@ size_t lumoto_partition(int *array, size_t low_part, size_t high_part)
  * quick_sort_recursive - sorts an int array in ascending order recursively
  *
  * @array: array to be sorted
- * @low_part: lower partition of array
- * @high_part: higher partition of array
+ * @s: size of the array
+ * @low_pt: lower partition of array
+ * @high_pt: higher partition of array
  *
  * Return: nothing
  */
-void quick_sort_recursive(int *array, size_t low_part, size_t high_part)
+void quick_sort_recursive(int *array, size_t low_pt, size_t high_pt, size_t s)
 {
-	size_t partition_ndx, sub_ndx;
+	size_t partition_ndx, sub_ndx, size;
 
-	if (low_part < high_part)
+	size = s;
+	if (low_pt < high_pt)
 	{
-		partition_ndx = lumoto_partition(array, low_part, high_part);
+		partition_ndx = lumoto_partition(array, low_pt, high_pt, size);
 
 		if (partition_ndx > 0)
 		{
 			sub_ndx = partition_ndx - 1;
-			quick_sort_recursive(array, low_part, sub_ndx);
+			quick_sort_recursive(array, low_pt, sub_ndx, size);
 		}
 
-		quick_sort_recursive(array, partition_ndx + 1, high_part);
+		quick_sort_recursive(array, partition_ndx + 1, high_pt, size);
 	}
 }
 
@@ -80,5 +90,5 @@ void quick_sort(int *array, size_t size)
 	if (!array || size < 2)
 		return;
 
-	quick_sort_recursive(array, 0, size - 1);
+	quick_sort_recursive(array, 0, size - 1, size);
 }
